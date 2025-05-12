1f7975205ab82ed7f4f3199ab40563d605c3fbfc
Asciugano <samuele.diaferio@fastwebnet.it>
2025-05-12 19:44:23 +0200

    aggiunto il swipe to delete e altre piccole migliorie

--- lib/views/pages/todo_page.dart
+++ lib/views/pages/todo_page.dart
@@ -2,23 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:todo_app_flutter/data/notifiers.dart';
 import 'package:todo_app_flutter/views/widgets/todo_item_widget.dart';
 
-class TodoPage extends StatelessWidget {
+class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
 
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: SingleChildScrollView(
-          child: Column(
-            children: List.generate(
-              todoListNotifier.value.length,
-              (i) => TodoItemWidget(item: todoListNotifier.value[i]),
+      body: ValueListenableBuilder(
+        valueListenable: todoListNotifier,
+        builder: (context, todoList, child) {
+          return Padding(
+            padding: EdgeInsets.all(10),
+            child: SingleChildScrollView(
+              child: AnimatedCrossFade(
+                firstChild: Column(
+                  children: List.generate(
+                    todoList.length,
+                    (i) => TodoItemWidget(item: todoList[i]),
+                  ),
+                ),
+                secondChild: Column(
+                  children: [
+                    Image.asset('assets/img/paperella_spaesata.png'),
+                    Text('Non c\'é nulla da fare'),
+                  ],
+                ),
+                crossFadeState:
+                    todoList.isEmpty
+                        ? CrossFadeState.showSecond
+                        : CrossFadeState.showFirst,
+                duration: Duration(milliseconds: 300),
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
--- lib/views/widgets/todo_item_widget.dart
+++ lib/views/widgets/todo_item_widget.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app_flutter/data/classes/todo_item.dart';
 import 'package:todo_app_flutter/data/constraints.dart';
 import 'package:todo_app_flutter/data/notifiers.dart';
@@ -15,61 +18,90 @@ class TodoItemWidget extends StatefulWidget {
 class _TodoItemWidgetState extends State<TodoItemWidget> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => print('cliccato ${widget.item.title}'),
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 2),
-        width: double.infinity,
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Checkbox(
-                  tristate: false,
-                  value: widget.item.isCompleted,
-                  onChanged:
-                      (bool? value) => setState(() {
-                        widget.item.isCompleted = value!;
-                      }),
-                ),
-                Column(
-                  children: [
-                    Text(
-                      widget.item.title,
-                      style: KTextStyle.titleText(
-                        themeColorNotifier.value,
-                        isCompleted: widget.item.isCompleted,
+    return Dismissible(
+      key: Key('item'),
+      onDismissed: (direction) => deleteItem(),
+      direction: DismissDirection.startToEnd,
+      background: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Container(
+          color: Colors.red,
+          alignment: Alignment.centerRight,
+          child: ListTile(title: Text('Elimina'), leading: Icon(Icons.delete)),
+        ),
+      ),
+      child: GestureDetector(
+        onTap: () => print('cliccato ${widget.item.title}'),
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 2),
+          width: double.infinity,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Checkbox(
+                    tristate: false,
+                    value: widget.item.isCompleted,
+                    onChanged:
+                        (bool? value) => setState(() {
+                          widget.item.isCompleted = value!;
+                        }),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        widget.item.title,
+                        style: KTextStyle.titleText(
+                          themeColorNotifier.value,
+                          isCompleted: widget.item.isCompleted,
+                        ),
                       ),
-                    ),
+                      Text(
+                        widget.item.desctiption,
+                        style: KTextStyle.descriptionText(
+                          isCompleted: widget.item.isCompleted,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(child: Container()),
+                  if (widget.item.scadenza != null)
                     Text(
-                      widget.item.desctiption,
+                      '${widget.item.scadenza!.day}/${widget.item.scadenza!.month}/${widget.item.scadenza!.year}',
                       style: KTextStyle.descriptionText(
                         isCompleted: widget.item.isCompleted,
                       ),
                     ),
-                  ],
-                ),
-                Expanded(child: Container()),
-                if (widget.item.scadenza != null)
+                  SizedBox(width: 10),
                   Text(
-                    '${widget.item.scadenza!.day}/${widget.item.scadenza!.month}/${widget.item.scadenza!.year}',
+                    widget.item.priorityLevel.label,
                     style: KTextStyle.descriptionText(
                       isCompleted: widget.item.isCompleted,
                     ),
                   ),
-                SizedBox(width: 10),
-                Text(
-                  widget.item.priorityLevel.label,
-                  style: KTextStyle.descriptionText(
-                    isCompleted: widget.item.isCompleted,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
+
+  void deleteItem() {
+    todoListNotifier.value.remove(widget.item);
+    saveTodo();
+  }
+  
+  Future saveTodo() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> jsonTodos =
+        todoListNotifier.value
+            .map((todo) => jsonEncode(todo.toJson()))
+            .toList();
+    print(jsonTodos);
+    await prefs.setStringList(KKeys.jsonTodos, jsonTodos);
+  }
+
 }
